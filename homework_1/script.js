4ef0879c419a32d488450ea4135d59931657fd29
Anton_Plsh <pilyushenko@ekosphere.ru>
2023-04-23 11:45:17 +0300

    Fixes
    Variable instead of valueOf Object property

--- homework_1/script.js
+++ homework_1/script.js
@@ -1,9 +1,8 @@
-const sum = function () {
-  let argument = arguments[0];
+const sum = function (argument) {
   if (argument) {
-    sum.valueOf = sum.valueOf >= 0 ? sum.valueOf + argument : argument;
+    this.a = this.a >= 0 ? this.a + argument : argument;
     return sum;
   } else {
-    return sum.valueOf;
+    return this.a;
   }
 };
